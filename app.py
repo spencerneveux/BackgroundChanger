3cb490999f2631b324db434b14a2bbc037bb14c8
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2019-08-15 18:30:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,54 @@
-import requests, webbrowser, ctypes, urllib.request 
+import requests, ctypes, urllib.request 
 from PIL import Image
+from unsplash import Unsplash
 
 # Path for the image
 path = "YourPath/unsplash.jpg"
 
-# Access Key for Unsplash API
-KEY = "Your key"
+# Instantiate Unsplash Object
+u = Unsplash()
 
-# Call API/jsonify data
-response = requests.get("https://api.unsplash.com/photos/random/?client_id=" + KEY).json()
-print(response)
-# Get the raw image from the call
-raw = response['urls']['full']
+# Photo
+photo = ""
 
-# Save the image locally
-urllib.request.urlretrieve(raw, "unsplash.jpg")
-print(raw)
+# Method to change the desktop image
+def change_desktop(photo):
+	# save image locally 
+	urllib.request.urlretrieve(photo, "unsplash.jpg")
+	print(photo)
 
-# Change background image with ctypes library, code = 20
-ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
+	# Change background image with ctypes library, code = 20
+	ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
+
+# -------------------
+# Menu
+# -------------------
+def menu():
+	print("Welcome!\nLet's change your background")
+	print("1) Random Photo\n2) Search by term\n3) Search by collection\n4) Exit")
+
+# -------------------
+# Main
+# -------------------
+def main():
+	flag = True
+	while (flag):
+		menu()
+		user_input = input()
+		if user_input == "1":
+			photo = u.get_random_image()
+			change_desktop(photo)
+		elif user_input == "2":
+			search_term = input("Search term: ")
+			photo = u.get_photo(search_term)
+			change_desktop(photo)
+		elif user_input == "3":
+			collection = input("Collection name: ")
+			photo = u.get_collection(collection)
+			change_desktop(photo)
+		elif user_input == "4":
+			flag = False
+			print("Exiting")
+
+if __name__ == '__main__':
+	main()
